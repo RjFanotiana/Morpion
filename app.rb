3602ab285a0938db4192149aea436e8d8b464410
RjFanotiana <46318282+RjFanotiana@users.noreply.github.com>
2019-03-05 11:04:20 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,5 @@
-
-8 lines (6 sloc) 176 Bytes
 require 'bundler'
 Bundler.require
-
-require 'lib/application'
-
-
-Application.new
-
+require_relative  'lib/Application'
+system("clear")
+Application.new.perform
