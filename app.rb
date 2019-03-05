75a021c0898576c2757452d7ee5850a14897da23
RjFanotiana <46318282+RjFanotiana@users.noreply.github.com>
2019-03-05 10:22:57 +0100

    update ap

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@
 8 lines (6 sloc) 176 Bytes
 require 'bundler'
 Bundler.require
-$:.unshift File.expand_path("./../lib", __FILE__)
-require 'app/application'
 
-#launching Application.new will start a new game
+require 'lib/application'
+
+
 Application.new
 
